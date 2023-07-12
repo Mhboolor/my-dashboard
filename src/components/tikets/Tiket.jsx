f7ed10081f1f37e33ce578c505981bf4fc553830
Mhboolor <mhstare81@gmail.com>
2023-07-12 03:39:15 +0330

    feat:Create Tiket Component

--- src/components/tikets/Tiket.jsx
+++ src/components/tikets/Tiket.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import {BsThreeDots} from "react-icons/bs"
 
 function Tiket() {
   return (
-    <div>Tiket</div>
-  )
+    <tr className="hover:bg-light">
+      <td className="p-3">#9852</td>
+      <td className="flex items-center gap-2 p-3">
+        <img
+          src="./images/users/user-1.jpg"
+          alt="user"
+          className="w-6 h-6 rounded-full"
+        />
+        <span>Debra J. Wilson</span>
+      </td>
+      <td>Your item has been updated!</td>
+      <td className="p-3">
+        <span className="bg-red/100 text-xs py-1 px-2 rounded-md text-white">
+          High
+        </span>
+      </td>
+      <td className="p-3">
+        <span className="bg-green/100 text-xs py-1 px-2 rounded-md text-white">
+          Open
+        </span>
+      </td>
+      <td className="p-3">01/04/2017 </td>
+      <td className="p-3">21/05/2017</td>
+      <td className="p-3">
+        <button className="bg-light p-3">
+          <BsThreeDots />
+        </button>
+      </td>
+    </tr>
+  );
 }
 
-export default Tiket
+export default Tiket;
