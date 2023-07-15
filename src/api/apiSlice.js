57cfeb2387be13b35e366cf69fba5f737f72caba
Mhboolor <mhstare81@gmail.com>
2023-07-15 04:13:21 +0330

    feat: add tagtype for refetching

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -3,16 +3,19 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:9000" }),
+  tagTypes : ["PRODUCTS"],
   endpoints: (builder) => ({
     getAllProducts : builder.query({
       query : () => "/products",
+      providesTags : ["PRODUCTS"]
     }),
     addProduct : builder.mutation({
       query : newProduct => ({
         url : "/products",
         method : "POST",
         body : newProduct
-      })
+      }),
+      invalidatesTags : ["PRODUCTS"]
     })
   }),
 });
